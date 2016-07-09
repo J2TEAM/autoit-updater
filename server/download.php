4f950778d89103e2204cd9db47985a79a7e5f5cf
J2TeaM <junookyo@gmail.com>
2016-07-10 04:03:03 +0700

    Update new server

---
+++ server/download.php
@@ -0,0 +1,35 @@
+<?php
+function downloadFile($file) { // $file = include path 
+  if(file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename=' . basename($file));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+    readfile($file);
+    exit;
+  }
+}
+
+function filter($fileName) {
+  $filter = array('..\\', '<', '>', ':', '"', '/', '\\', '|', '?', '*');
+  return str_replace($filter, '', $fileName);
+}
+
+if (isset($_GET['channel'], $_GET['version'], $_GET['file']) && ! empty($_GET['file'])) {
+  $channel = strtolower($_GET['channel']);
+  if (in_array($channel, array('stable', 'beta')) && ! empty($_GET['version'])) {
+    $slash = DIRECTORY_SEPARATOR;
+
+    // download.php?channel=stable&version=1.0.0&file=setup.exe
+    $path = __DIR__ . $slash . 'files' . $slash . $channel . $slash . $_GET['version'] . $slash . filter($_GET['file']);
+    downloadFile($path);
+  }
+}
+
+exit('error');
---
+++ server/version.php
@@ -0,0 +1,60 @@
+<?php
+define('FILES_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR);
+
+if ( ! file_exists(FILES_DIR)) {
+  mkdir(FILES_DIR);
+}
+
+function getChangelog($file) {
+  return (file_exists($file)) ? file_get_contents($file) : NULL;
+}
+
+function showData($data) {
+  header('Content-Type: application/vnd.api+json');
+  echo json_encode(array('data' => $data));
+  exit;
+}
+
+if (isset($_SERVER['HTTP_USER_AGENT']) AND strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'autoit updater') !== FALSE) { 
+  if (isset($_GET['channel'])) {
+    $channel = strtolower($_GET['channel']);
+
+    if (file_exists(FILES_DIR . $channel)) {
+      // Get latest version
+      $dirs = scandir(FILES_DIR . $channel, 1);
+      $dirs = array_filter($dirs, function($file) {
+        return ( ! in_array($file, array('.', '..')) && is_dir(FILES_DIR . strtolower($_GET['channel']) . DIRECTORY_SEPARATOR . $file));
+      });
+      $latestVersion = array_shift($dirs);
+      unset($dirs);
+
+      // Get setup and changelog
+      $path = FILES_DIR . $channel . DIRECTORY_SEPARATOR . $latestVersion . DIRECTORY_SEPARATOR;
+      $dirs = scandir($path);
+      $txt = array_filter($dirs, function($file) {
+        $extension = explode('.', $file);
+        return strtolower(end($extension)) === 'txt';
+      });
+      $exe = array_filter($dirs, function($file) {
+        $extension = explode('.', $file);
+        return strtolower(end($extension)) === 'exe';
+      });
+
+      if (count($txt) > 0 && count($exe) > 0) {
+        $changelog = array_shift($txt);
+        $setup = array_shift($exe);
+        unset($txt, $exe);
+
+        $data = array(
+          'version'   => $latestVersion,
+          'name'      => $setup,
+          'changelog' => getChangelog($path . $changelog)
+        );
+
+        showData($data);
+      }
+    }
+  }
+}
+
+showData(NULL);
